d6a3a48a73d3eaaf9c2295f18c768a57e4a96793
react-seawind <rajan@seawind.in>
2024-03-29 11:07:50 +0530

    changes

--- BCP/src/Private/Requiredau.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from 'react-router-dom';
-export default function RequireAuth({ children }) {
-  const currentUser = sessionStorage.getItem('logindata');
-
-  return currentUser ? children : <Navigate to="/login" replace />;
-}
--- BCP/src/components/EDITOR/NewEditor.jsx
+++
@@ -1,9 +0,0 @@
-// NewEditor.js
-import React from 'react';
-import TinyMCEEditor from './Editor';
-
-const NewEditor = ({ onChange }) => {
-  return <TinyMCEEditor onChange={onChange} />;
-};
-
-export default NewEditor;
